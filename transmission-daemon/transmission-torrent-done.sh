389719790909c6032f4ad1e9efccf15dc96ba532
Walter Ferreira <wferreira.uy@gmail.com>
2023-02-17 20:25:07 -0300

    Cleaning script

--- transmission-daemon/transmission-torrent-done.sh
+++ transmission-daemon/transmission-torrent-done.sh
@@ -25,7 +25,6 @@ echo "Output file: $OUT_FILE"
 
 # If the video file has an EAC3 audio encoding, then re-encode to AC3.
 pushd $TR_TORRENT_DIR
-
 if mediainfo $IN_FILE|grep EAC3; then
     echo -e "Found EAC3 audio encoding.\nRe-encondig to AC3:"
     $(ffmpeg -hwaccel auto -y -i $IN_FILE -map 0 -c:s copy -c:v copy -c:a ac3 -b:a 640k $OUT_FILE) && rm -f $IN_FILE
@@ -33,7 +32,6 @@ else
     echo -e "No EAC3 encondig found.\nSkipping audio re-encoding.\n"
 fi
 popd
-pwd
 
 # Remove torrent file
 transmission-remote -n user:123456 -t $TR_TORRENT_ID -r
